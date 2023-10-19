41671fc9939d8b892828a0bf5ebb872e7cccaef1
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-10-20 12:23:24 +1300

    test deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   // configure webpack to be able to render pdfs
-  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
-    config.resolve.alias.canvas = false
-    config.resolve.alias.encoding = false
-    return config
-  }
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
